276acfec2005de025cb787c8180268a962b35e20
[Mateusz] <[mhalman094@gmail.com]>
2018-08-28 15:48:12 +0200

    refactor

--- assets/js/navCollapse.js
+++ assets/js/navCollapse.js
@@ -1,45 +1,21 @@
 
-// class NavbarCollapse {
-//   constructor() {
-//     this.init();
-//     this.toggleMenu();
-//   }
-//   init() {
-//     this.hambIcon = document.querySelector('nav  .nav-content .btn-nav .box');
-//     this.menu = document.querySelector('#header .nav-collapse');
-//   }
-
-//   toggleMenu() {
-//     let clicker = 1;
-//     this.hambIcon.addEventListener('click', () => {
-//       if (clicker % 2 === 0) {
-//         this.menu.style.marginLeft = '-200px';
-//       }else {
-//         this.menu.style.marginLeft = 0;
-//       }
-//       clicker += 1;
-//       console.log(clicker);
-//     });
-//   }
-// }
-
-// const navbarCollapse = new NavbarCollapse();
-
-$(document).ready(()=>{
+$(document).ready(() => {
   const hamburger = $('.btn-nav .box');
   const menu = $('.nav-collapse');
-  const close =$('.nav-collapse i');
+  const close = $('.nav-collapse i');
+  const title = $('.title');
+  const logo = $('.nav-content img');
 
-  hamburger.click(()=>{
-    $('.title').toggleClass('blur')
-    $('.nav-content img').toggleClass('blur');
+  hamburger.click(() => {
+    title.toggleClass('blur')
+    logo.toggleClass('blur');
     menu.slideToggle();
     hamburger.fadeToggle();
   });
-  close.click(()=>{
+  close.click(() => {
     menu.slideToggle();
-    $('.title').toggleClass('blur');
-    $('.nav-content img').toggleClass('blur');
+    title.toggleClass('blur');
+    logo.toggleClass('blur');
     hamburger.fadeToggle();
   });
 });
